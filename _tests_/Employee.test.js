debd4251728d3770be09ce69e33f497177d89379
Allure <samira-h@hotmail.co.uk>
2022-03-05 21:42:54 +0000

    employee test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee Class', () => {
+    describe('Initialization', () => {
+        it('Creates an object with a name and an id.', () => {
+            const employee = new Employee(123, 'Dave')
+
+            expect(employee.id).toEqual(123);
+            expect(employee.name).toEqual('Dave');
+        });
+    });
+});
