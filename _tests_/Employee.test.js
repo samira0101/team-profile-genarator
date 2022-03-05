015d54a96e34c590fef52666d512427a2a656e38
Allure <samira-h@hotmail.co.uk>
2022-03-05 22:59:10 +0000

    employee testing

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee');
 
 describe('Employee Class', () => {
     describe('Initialization', () => {
-        it('Creates an object with a name and an id.', () => {
+        it('Createss an object with a name and an id.', () => {
             const employee = new Employee(123, 'Dave')
 
             expect(employee.id).toEqual(123);
