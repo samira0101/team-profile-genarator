bdb2534a476afd71fb08fbcb0e9a01904acc6d3f
Allure <samira-h@hotmail.co.uk>
2022-06-03 16:04:22 +0100

    creates Intern card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -35,3 +35,22 @@ const generateEngineer = function (engineer) {
   </div>
   `
 }
+
+// create Intern card 
+const generateIntern = function (intern) {
+  return `
+  <div class="col-4 mt-4">
+      <div class="card h-100">
+          <div class="card-header">
+              <h3>${intern.name}</h3>
+              <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+          </div>
+          <div class="card-body">
+              <p class="id">ID: ${intern.id}</p>
+              <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+              <p class="school">School: ${intern.school}</p>
+          </div>
+  </div>
+</div>
+  `
+};
