6a022b5f23958e2971940e8b18a560520085dce9
Allure <samira-h@hotmail.co.uk>
2022-06-03 15:56:12 +0100

    creates employee object

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,9 +1,14 @@
+// using Employee constructor 
 const Employee = require('../lib/Employee');
 
-describe('Employee Class', () => {
-    describe('Initialization', () => {
-        it('Createss an object with a name and an id.', () => {
-            const employee = new Employee(123, 'Dave')
+// creates employee object 
+test('creates an employee object', () => {
+    const employee = new Employee('Samira', 90, 'mhsamira.21@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
 
             expect(employee.id).toEqual(123);
             expect(employee.name).toEqual('Dave');
