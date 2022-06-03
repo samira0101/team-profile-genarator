502f87b1489def398087915d60b0e08c2a4188a5
Allure <samira-h@hotmail.co.uk>
2022-06-03 16:03:59 +0100

    creates Engineer card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,3 +17,21 @@ const generateManager = function (manager) {
   `;
 }
 
+// create Engineer card
+const generateEngineer = function (engineer) {
+  return `
+  <div class="col-4 mt-4">
+      <div class="card h-100">
+          <div class="card-header">
+              <h3>${engineer.name}</h3>
+              <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+          </div>
+          <div class="card-body">
+              <p class="id">ID: ${engineer.id}</p>
+              <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+              <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+          </div>
+      </div>
+  </div>
+  `
+}
