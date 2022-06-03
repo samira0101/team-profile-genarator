021020f6b070fbf22c648b41cfae21d1eae8f0ed
Allure <samira-h@hotmail.co.uk>
2022-06-03 15:50:08 +0100

    linking to pages

---
+++ index.js
@@ -0,0 +1,11 @@
+// link to page creation
+const generateHTML = require('./src/generateHTML');
+
+// team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
+// node modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
